5f654b31c4e2ca273751a6daaaed0cfea662c3f2
margo10011990 <margo10011990@gmail.com>
2021-04-02 13:04:01 +0300

    added validation to forms

--- src/app/features/authorization/login/login.component.ts
+++ src/app/features/authorization/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,13 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
   public hide = true;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initLoginForm();
+  }
+
+  public initLoginForm(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+    });
+  }
 
   public onToggleClickPasswordIcon(): void {
     this.hide = !this.hide;
+  }
+
+  public onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('submit: ', this.loginForm.value);
+    }
   }
 }
--- src/app/features/authorization/register/register.component.ts
+++ src/app/features/authorization/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,12 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   public hide = true;
+  public registerForm: FormGroup;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initRegisterForm();
+  }
+
+  public initRegisterForm(): void {
+    this.registerForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+    });
+  }
 
   public onToggleClickPasswordIcon(): void {
     this.hide = !this.hide;
+  }
+
+  public onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log('submit: ', this.registerForm.value);
+    }
   }
 }
